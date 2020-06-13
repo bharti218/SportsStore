d208bbb00f28ae0fd98719455c9fc18c47501b64
bharti_218 <bhartisingh5492@gmail.com>
2020-06-13 21:20:10 +0530

    Setting up project which is working fine with mock data. Now Adding database.

---
+++ SportsStore.Domain/Abstract/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Abstract
+{
+    public interface IProductRepository
+    {
+        // this interface uses IEnumerable<T> to allow a caller to obtain a sequence of product objects without saying how 
+        // or where the data is stored or retrieved.
+        IEnumerable<Product> Products { get; set; }
+    }
+}
